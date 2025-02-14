6c2fae327e846227c340aa113c07561ffa616cf6
kzmsnj <farrasarkanwardana@gmail.com>
2025-02-14 21:20:23 +0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,37 @@
+
+import picture from "./picture.png";
+import aquarium1 from "./aquarium1.jpg";
+import aurora2 from "./aurora2.jpg";
+
+import seasons from "./seasons.jpg";
+import dewa from "./dewa.jpg";
+import onbended from "./onbended.jpg";
+import herewithme from "./herewithme.jpg";
+import givemeyour from "./givemeyour.jpg";
+import honey from "./honey.jpg";
+import always from "./always.jpg";
+import mylove from "./mylove.jpg";
+
+
+import fireworks from "./fireworks.gif";
+import mocha from "./mocha.gif";
+
+
+
+
+export {
+
+picture,
+aquarium1,
+aurora2,
+seasons,
+mocha,
+fireworks,
+dewa,
+onbended,
+herewithme,
+givemeyour,
+honey,
+always,
+mylove,
+};
